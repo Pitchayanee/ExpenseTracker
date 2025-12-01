41133887559ab5f5f2c1a55ea9bb5eb809eed936
Pitchayanee Phokhem <163176723+Pitchayanee@users.noreply.github.com>
2025-12-01 09:43:08 +0700

    Wrap App component with HashRouter

--- src/main.jsx
+++ src/main.jsx
@@ -1,12 +1,15 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import { HashRouter } from 'react-router-dom';
 import App from './App.jsx';
-import { ExpensesProvider } from './context/ExpensesProvider.jsx'; 
+import { ExpensesProvider } from './context/ExpensesProvider.jsx';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <ExpensesProvider>
-      <App />
+      <HashRouter>
+        <App />
+      </HashRouter>
     </ExpensesProvider>
-  </React.StrictMode>,
+  </React.StrictMode>
 );
